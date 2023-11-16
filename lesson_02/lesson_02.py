3f83b9a9c1d8fe9d0c150c292fcfe1ba7d06c265
OPanchenko <pan.pan.4it@gmail.com>
2023-11-16 21:20:12 +0200

    lesson 2

--- lesson_02/lesson_02.py
+++ lesson_02/lesson_02.py
@@ -21,7 +21,17 @@ second_line = ("asdfghjklqwertyu"
     "1234567890"
     ")(*&^%#%#&*&^%??><<@!)"
     "djfhdjd,f")
-print(second_line)
+#print(second_line)
+'''
+False      await      else       import     pass
+None       break      except     in         raise
+True       class      finally    is         return
+and        continue   for        lambda     try
+as         def        from       nonlocal   while
+assert     del        global     not        with
+async      elif       if         or         yield
+'''
+
 # Оператори дій
 1+1
 1-2
@@ -40,6 +50,14 @@ print(second_line)
 &=   |=   ^=   >>=  <<=  **=
 """
 #  '  "  \
+result  = (2 + 3) * 4
+print(result)
+
+my_list = [1, 2, 3, 5]
+print(my_list)
+
+my_dict = {'ключ': 'значення'}; my_dict2 = {} # bad example of code
+
 
 # Літерали
 42
@@ -50,34 +68,60 @@ more Python"""
 
 # Типи даних
 ## Цілі числа
-aples = 5
-users = 2
+aples = 22
+users = 7
 count_aples = aples / users
 print(count_aples)
+
+one_number = 42
+vosm_number = 0o52
+sx_number = 0x2A
+bin_num = 0b101010
+# 9,223,372,036,854,775,807
+print(one_number, vosm_number, sx_number, bin_num)
+
+print(1.0E100)
+
+print(3.1428571428571 == aples / users)
 
 ## Строки
 hello = "hello"
 word = 'word'
 english_words = 'I don\'t know'
-print(english_words)
+print(type(english_words))
+print(english_words[2])
+# english_words[2] = "t"  # ПОМИЛКА!!!!
 ## Кортежі (Tuples)
-(0, 2, 4, 6)
+my_tuple = (0, 2, 4, 6)
+print(my_tuple[0])
+# my_tuple[0] = 1  # ПОМИЛКА!!!!
+
 ## Списки [Lists]
 new_list = ["a", 0, 1.2, hello]
-print(new_list)
+print(new_list[-1])
+hello = "new_value"
+new_list[-1] = hello
+print(new_list[-1])
+alphabet = ["a", "b", "c", "a"]
+print(alphabet)
+#print(new_list)
 ## Набори {Sets}
-simple_set = { 1, 2, 3, 1}
+simple_set = {1, 2, 3, 1}
 print(simple_set)
 ## Словники {Dictionaries: 2}
 samle_dict = {"key": "value"}
+print(samle_dict["key"])
+
 ## None
 a = None
 ## Булеві значення
 True
 False
+
 1 == 1
 2 > 56
 # "a" < 45  # різні типи - помилка!!!
+
 # Змінні у Пітоні
 storona_1 = 1
 storona_2 = 1 + 1
@@ -104,7 +148,8 @@ print(s2)
 s3 = 22 % 7
 print(s3)
 s4 = 22 - (7 * s2)
-print(s4 == s3)
+
+#print(s4 == s3)
 ## Множення, додавання, віднімання
 x = 2
 x2 = x * 2
@@ -114,10 +159,15 @@ print("sa"+"sha")
 
 
 ## Піднесення у степінь
-print (2**8)
+print(2**8)
 ## Порівняння
-(1 < 2) and (2 < 3)
-(2 < 1) or (2 < 3)
+result = (1 < 2) and (2 < 3)
+print(result)
+print((2 < 1) or (2 < 3))
+x = min(1, 2, 3)
+y = max(1, 2, 3)
+print(x, y)
+
 # Ввод та вивід даних
 # print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
 ## f-string
@@ -135,7 +185,7 @@ for x in range(1, 11):
     print('{0:2d} {1:4d} {2:6d}'.format(x, x*x, x*x*x))
 
 ## concatenate
-f_list = ["aple", "banana", "kivi"]
+f_list = ["apple", "banana", "kivi"]
 fruits = ", ".join(f_list)
 print(fruits)
 
