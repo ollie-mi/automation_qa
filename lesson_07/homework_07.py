53619dd3aa106bf52f55db7c0f49c0004f555b82
Olha Mialkivska <olha.mialkivska@geozilla.com>
2023-12-06 23:49:02 +0000

    homework 07

--- lesson_07/homework_07.py
+++ lesson_07/homework_07.py
@@ -3,21 +3,25 @@
 лише до максимального значення для добутку - 25.
 Код майже готовий, треба знайти помилки та випраавити\доповнити.
 """
-def multiplication_table(number):
+
+
+def multiplication_table(number: int):
+    """Prints multiplication table with max result of multiplication of 25"""
     # Initialize the appropriate variable
     multiplier = 1
 
     # Complete the while loop condition.
-    while multiplier <= number:
+    while True:
         result = number * multiplier
         # десь тут помила, а може не одна
-        if  result > "25":
+        if result > 25:
             # Enter the action to take if the result is greater than 25
-            pass
+            break
         print(str(number) + "x" + str(multiplier) + "=" + str(result))
 
         # Increment the appropriate variable
-        multi += 1
+        multiplier += 1
+
 
 multiplication_table(3)
 # Should print:
@@ -33,33 +37,83 @@ multiplication_table(3)
 """
 
 
+def two_numbers_sum(num_1: int, num_2: int) -> int:
+    """Calculate the sum of two numbers"""
+    return num_1 + num_2
+
+
+print("3 + 5 =", two_numbers_sum(3, 5))
+
+
 # task 3
 """  Написати функцію, яка розрахує середнє арифметичне списку чисел.
 """
+
+
+def average_of_list_numbers(number_list: list) -> float:
+    """Calculate arithmetic mean of list of numbers"""
+    return sum(number_list) / len(number_list)
+
+
+print("Average of the list:", average_of_list_numbers([1, 2, 3, 4, 5, 6]))
+
 
 # task 4
 """  Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
 """
 
+
+def reverse_string(string: str) -> str:
+    """Reverse the given string"""
+    result_list = []
+    for i in reversed(range(len(string))):
+        result_list.append(string[i])
+    result_string = ''.join(result_list)
+    return result_string
+
+
+print("Reverse of Hello:", reverse_string('Hello'))
+
+
 # task 5
 """  Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
 """
+
+
+def longest_word_in_list(words_list: list) -> str:
+    """Finds the longest word in the list"""
+    longest_word = words_list[0]
+    for word in words_list:
+        if len(word) > len(longest_word):
+            longest_word = word
+    return longest_word
+
+
+capital_cities = ["Kyiv", "Wellington", "Reykjavik", "Zagreb", "Lisbon"]
+print("Longest word in the list:", longest_word_in_list(capital_cities))
+
 
 # task 6
 """  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
 у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
 не є підрядком першого рядка."""
-def find_substring(str1, str2):
 
-    return -1
+
+def find_substring(str1: str, str2: str) -> int:
+    """Finds position of string 1 in string 2. If fails to find, returns -1"""
+    if str2 in str1:
+        return str1.find(str2)
+    else:
+        return -1
+
 
 str1 = "Hello, world!"
 str2 = "world"
-print(find_substring(str1, str2)) # поверне 7
+print(find_substring(str1, str2))  # поверне 7
 
 str1 = "The quick brown fox jumps over the lazy dog"
 str2 = "cat"
-print(find_substring(str1, str2)) # поверне -1
+print(find_substring(str1, str2))  # поверне -1
 
 # task 7
 # task 8
@@ -69,3 +123,55 @@ print(find_substring(str1, str2)) # поверне -1
 перетворіть їх у 4 функції, що отримують значення та повертають результат.
 Обоязково документуйте функції та дайте зрозумілі імена змінним.
 """
+
+
+def duplicate_list_checker(ch_list: list) -> bool:
+    """Checks the list for duplicates"""
+    ch_set = set(ch_list)
+    if len(ch_list) > len(ch_set):
+        return True
+    return False
+
+
+big_list = [3, 5, -2, -1, -3, 0, 1, 4, 5, 2]
+print("Duplicate checker:", duplicate_list_checker(big_list))
+
+
+def key_value_swapper(dictionary: dict) -> dict:
+    """Swaps key and value in the dictionary"""
+    new_dict = {}
+    for key, value in dictionary.items():
+        new_key = value if isinstance(value, str) else str(value)
+        new_dict[new_key] = key
+    return new_dict
+
+
+base_dict = {'contry': 'Ukraine', 'continent': 'Europe', 'size': 123}
+print("Swap key and value in dict:", key_value_swapper(base_dict))
+
+
+def pizza_constructor() -> list:
+    """Adds toppings to pizza"""
+    pizza_topping = ''
+    toppings_list = []
+    while pizza_topping != 'quit':
+        pizza_topping = input("Please, write what you want to add to your pizza or write 'quit' to finish ")
+        if pizza_topping != 'quit':
+            toppings_list.append(pizza_topping)
+            print(f"We will add {pizza_topping} to your pizza")
+        if not toppings_list:
+            toppings_list.append("just cheese")
+    return toppings_list
+
+
+toppings_str = ', '.join(pizza_constructor())
+print("Your pizza contains:", toppings_str)
+
+
+def even_numbers_squares(num: list) -> list:
+    """Finds a list of squares of even numbers from the list."""
+    return [x ** 2 for x in num if x % 2 == 0]
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Even number squares:", even_numbers_squares(numbers))
