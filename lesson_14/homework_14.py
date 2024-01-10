5589caf77fef9bf708a6a7ce0128d79039539975
Olha Mialkivska <olha.mialkivska@geozilla.com>
2024-01-10 22:42:03 +0000

    hm-14 add SiteUser class

--- lesson_14/homework_14.py
+++ lesson_14/homework_14.py
@@ -30,3 +30,71 @@ else:
 
 Написати на це все тести
 """
+import re
+
+
+class SiteUser:
+
+    def __init__(self, name, email, access_level):
+        self.__name = name
+        self.__email = email
+        self.__access_level = access_level
+        self.__logcount = 0
+        self.__levels = ['admin', 'moderator', 'user']
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str):
+        if not isinstance(new_name, str):
+            raise ValueError("Name should be a string")
+        self.__name = new_name
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, new_email):
+        if not re.match(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$", new_email):
+            raise ValueError("Email address is not valid")
+        self.__email = new_email
+
+    @property
+    def access(self):
+        return self.__access_level
+
+    @access.setter
+    def access(self, new_level):
+        if new_level not in self.__levels:
+            raise ValueError("Such level doesn't exists")
+        self.__access_level = new_level
+
+    @property
+    def logcount(self):
+        return self.__logcount
+
+    def update_logcount(self):
+        self.__logcount += 1
+
+    def __str__(self):
+        return str(f"Користувач: {self.name}, Електронна пошта: {self.email}, Рівень доступу: {self.access}")
+
+    def __eq__(self, other):
+        if isinstance(other, SiteUser):
+            return self.__access_level == other.__access_level
+        return False
+
+
+if __name__ == '__main__':
+    user1 = SiteUser("John Doe", "john.doe@example.com", "user")
+    user2 = SiteUser("Jane Smith", "jane.smith@example.com", "admin")
+    user3 = SiteUser("John Kennedy", "john.kennedy@example.com", "user")
+    print(user1)
+
+    if user1 == user2:
+        print("Користувачі однакові")
+    else:
+        print("Користувачі різні")
