fc40a563a675040888376f5dd275ebd802cd5c4d
OPanchenko <pan.pan.4it@gmail.com>
2024-03-07 20:10:55 +0200

    lesson 0

---
+++ lesson_00/lesson.py
@@ -0,0 +1,13 @@
+
+my_word = "Welcome to Ukraine!"
+print(my_word)
+my_word = "Hello world!"
+print(my_word)
+
+
+def split_to_letter(my_word):
+    for leter in my_word:  # тут розбиваємо змінну на елементи
+        print(leter)  # тут виводимо  кожен елемент
+
+
+split_to_letter(my_word)
